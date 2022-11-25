7a93c1bf6d894375a75cb25e9c39b7466472b9dd
SEH <02milkyway13@naver.com>
2022-11-25 12:52:42 +0900

    useDAO, facilitiesDAO 수정! 시설등록시 테이블에 시설+회차 데이터 들어가도록 수정했습니다.

--- src/main/java/kr/co/dongdong/dao/UseDAO.java
+++ src/main/java/kr/co/dongdong/dao/UseDAO.java
@@ -93,6 +93,87 @@ import kr.co.dongdong.vo.UseVO;
 		return vo;
 	}
 	
+	public UseVO getUseTime(int facno, int restime) {
+		sb.setLength(0);
+		sb.append("SELECT * FROM use ");
+		sb.append("WHERE facno = ? and restime = ? ");
+		
+		UseVO vo = null;
+		
+		try {
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, facno);
+			pstmt.setInt(2, restime);
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				String usetime = rs.getString("usetime");
+				
+				vo = new UseVO(facno, restime, usetime);
+						
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return vo;
+	}
+	
+	public ArrayList<UseVO> getTime(int facno, String resdate) {
+		sb.setLength(0);
+		sb.append("select * from ( " );
+		sb.append("select restime, usetime from use ");
+		sb.append("where facno = ? and restime not in ");
+		sb.append("(select restime from reserve where resdate = ? and facno = ? ) ");
+		sb.append(") order by restime asc ");
+		
+		ArrayList<UseVO> list = new ArrayList<UseVO>();
+		
+		try {
+			pstmt = conn.prepareStatement(sb.toString());
+			pstmt.setInt(1, facno);
+			pstmt.setString(2, resdate);
+			pstmt.setInt(3, facno);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				
+				int restime = rs.getInt("restime");
+				String usetime = rs.getString("usetime");
+				
+				UseVO vo = new UseVO(facno, restime, usetime);
+				list.add(vo);
+						
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+		
+	}
+		// 회차 추가
+		public void insertOne(int facno, int restime, String usetime) {
+			sb.setLength(0);
+			sb.append("INSERT INTO use ");
+			sb.append("VALUES (?, ?, ?)");
+			
+			try {
+				pstmt = conn.prepareStatement(sb.toString());
+				pstmt.setInt(1, facno);
+				pstmt.setInt(2, restime);
+				pstmt.setString(3, usetime);
+				
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	
 	// 자원반납
 	public void close() {
 		try {
